80d515c3f2508362b4b2898975fc455a9e2f8628
Brittney <brittney.argirakis@gmail.com>
2024-05-17 08:59:26 -0400

    F - Added test check and random url generating code

--- shortener/utils/short_link.py
+++ shortener/utils/short_link.py
@@ -1,4 +1,10 @@
+import random
+import string
 
+NUMBERS = ["0","1","2","3","4","5","6","7","8","9"]
+LETTERS = list(string.ascii_lowercase)
 
 def build_short_link(long_link: str) -> str:
-    return "foo"
+    random_out = random.choices(NUMBERS + LETTERS, k=5)
+    generated_url = "".join(random_out)
+    return generated_url
--- test/test_short_link.py
+++ test/test_short_link.py
@@ -10,3 +10,7 @@ class ShortLinkTest(TestCase):
         short_link = build_short_link(long_link)
         # Outcome: we expect to get a 5 character A-Z0-9 string
         self.assertTrue(short_link.isalnum())
+
+        second_long_link = "another-example.com"
+        second_short_link = build_short_link(second_long_link)
+        self.assertNotEqual(second_short_link, short_link)
