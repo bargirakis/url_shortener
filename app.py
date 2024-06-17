2c406b8f1caafdc05af46a91337a14b462a9b7db
Brittney <brittney.argirakis@gmail.com>
2024-06-17 08:49:14 -0400

    F - configured and initialized db

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for
 from markupsafe import escape
+from shortener.models import db
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
