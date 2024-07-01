abe3f977790a7dd763a6c0efc170ae4ffbcf1601
Brittney Argirakis <brittney.argirakis@gmail.com>
2024-07-01 19:51:38 -0400

    clone to github

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from shortener.models import Url
 from flask import Flask, url_for
 from flask_migrate import Migrate
 from markupsafe import escape
+import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
 from db import db
 
 app = Flask(__name__)
@@ -9,10 +11,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
+class Url(db.Model):
+
+    __tablename__ = "url"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    short_url: Mapped[str] = mapped_column(String(5))
+    long_url: Mapped[str] = mapped_column(String(2000))
+    created: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.UTC))
+
+    def __repr__(self) -> str:
+        return f"URL(id={self.id!r}, short_url={self.short_url!r}, long_url={self.long_url!r})"
+
 with app.app_context():
     db.create_all()
-
-migrate = Migrate(app, db)
 
 @app.route("/")
 def hello_world():
--- migrations/env.py
+++ migrations/env.py
@@ -34,6 +34,8 @@ def get_engine_url():
 
 # add your model's MetaData object here
 # for 'autogenerate' support
+from db import Base
+target_metadata = Base.metadata
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 config.set_main_option('sqlalchemy.url', get_engine_url())
